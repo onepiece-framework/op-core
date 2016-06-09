a6a58d95fe2d813410c39507e1f50892b79cd2e7
Tomoaki Nagahara <1668339+TomoakiNagahara@users.noreply.github.com>
2016-06-09 17:50:39 +0900

    New: Autoloader.class.php

---
+++ Autoloader.class.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Autoloader.class.php
+ *
+ * @creation  2014-11-29
+ * @rebirth   2016-06-09
+ * @version   1.0
+ * @package   core7
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/**
+ * Autoloader
+ *
+ * @creation  2014-11-29
+ * @rebirth   2016-06-09
+ * @version   1.0
+ * @package   core7
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Autoloader
+{
+	static function Autoload( $class_name )
+	{
+		global $_OP;
+
+		//	Correspond namespace.
+		$class_name = str_replace('OP\\','',$class_name);
+
+		//	Generate file name;
+		$file_name = "{$class_name}.class.php";
+
+		//	Init include path.
+		$include_path = array();
+
+		//	Current directory.
+		$include_path[] = '.';
+
+		//	Add op-core's root.
+		$include_path[] = $_OP['OP_ROOT'];
+
+		//	Challenge to include.
+		foreach( $include_path as $path ){
+			$file_path = rtrim($path, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.$file_name;
+			if( $io = file_exists($file_path) ){
+				$io = include_once($file_path);
+			}
+		}
+	}
+}
--- Bootstrap.php
+++ Bootstrap.php
@@ -2,12 +2,24 @@
 /**
  * Bootstrap.php
  *
- * @created   2016-06-09
+ * @creation  2015-12-10
+ * @rebirth   2016-06-09
  * @version   1.0
  * @package   core7
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
+
+/**
+ * @see    http://qiita.com/devneko/items/ee83854eb422c352abc8
+ * @param  mixed $check
+ * @param  mixed $alternate
+ * @return mixed
+ */
+function ifset(&$check, $alternate = NULL)
+{
+	return (isset($check)) ? $check : $alternate;
+}
 
 //	Check mbstring installed.
 if(!function_exists('mb_language') ){
@@ -26,7 +38,7 @@ $_OP['OP_ROOT'] = __DIR__.'/';
 $_OP['APP_ROOT'] = dirname($_SERVER['SCRIPT_FILENAME']).'/';
 
 //	Register autoloader.
-include(__DIR__.'/Autoloader.php');
+include(__DIR__.'/Autoloader.class.php');
 if(!spl_autoload_register('Autoloader::Autoload',true,true)){
 	function __autoload($class){
 		Autoloader::Autoload($class);
