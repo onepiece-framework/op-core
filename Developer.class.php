8d13c68a07853498d194600d145a2536dbb3cce2
Tomoaki Nagahara <1668339+TomoakiNagahara@users.noreply.github.com>
2016-06-15 18:06:31 +0900

    New: Developer.class.php

---
+++ Developer.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Developer.class.php
+ *
+ * @creation  2016-06-09
+ * @version   1.0
+ * @package   core7
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/**
+ * Developer
+ *
+ * @creation  2014-11-29
+ * @version   1.0
+ * @package   core7
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Developer extends OP
+{
+
+}
