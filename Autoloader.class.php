d9c5482923c3a7bbc2d3e9303f5a75c4452b51e0
Tomoaki Nagahara <1668339+TomoakiNagahara@users.noreply.github.com>
2016-11-17 15:30:59 +0900

    Fix: Autoloader - Init include path is first time only.

--- Autoloader.class.php
+++ Autoloader.class.php
@@ -23,31 +23,37 @@
 class Autoloader
 {
 	/**
+	 * @var array
+	 */
+	static $_include_path;
+
+	/**
 	 * Autoload.
 	 *
 	 * @param string $class_name
 	 */
-	static function Autoload( $class_name )
+	static function Autoload($class_name)
 	{
 		global $_OP;
 
 		//	Correspond namespace.
-		$class_name = str_replace('OP\\','',$class_name);
+		$class_name = str_replace('OP\\', '', $class_name);
 
 		//	Generate file name;
 		$file_name = "{$class_name}.class.php";
 
-		//	Init include path.
-		$include_path = array();
+		//	Initialization is only the first.
+		if(!self::$_include_path ){
 
-		//	Current directory.
-		$include_path[] = '.';
+			//	Current directory.
+			self::$_include_path[] = '.';
 
-		//	Add op-core's root.
-		$include_path[] = $_OP['OP_ROOT'];
+			//	Add op-core's root.
+			self::$_include_path[] = $_OP['OP_ROOT'];
+		}
 
 		//	Challenge to include.
-		foreach( $include_path as $path ){
+		foreach( self::$_include_path as $path ){
 			$file_path = rtrim($path, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.$file_name;
 			if( $io = file_exists($file_path) ){
 				$io = include_once($file_path);
