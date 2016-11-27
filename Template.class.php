f6e406bbe9ac1949688560126a753068c63fe596
Tomoaki Nagahara <1668339+TomoakiNagahara@users.noreply.github.com>
2016-11-28 02:26:32 +0900

    New: Template

---
+++ Template.class.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Template.class.php
+ *
+ * @rebirth   2016-11-26
+ * @version   1.0
+ * @package   core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/**
+ * Template
+ *
+ * @rebirth   2016-11-26
+ * @version   1.0
+ * @package   core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Template extends OnePiece
+{
+	/**
+	 * Get template content.
+	 *
+	 * @param  string $path
+	 * @param  array  $args
+	 * @return string
+	 */
+	static function Get($path, $args=null)
+	{
+		//	...
+		if(!$path){
+			Notice::Set('$path is empty.');
+			return null;
+		}
+
+		//	Start buffering.
+		if(!ob_start()){
+			Notice::Set("ob_start is failed.");
+			return null;
+		}
+
+		// Extract array.
+		if( is_array($args) and count($args) ){
+			if(isset($args[0])){
+				Notice::Set('Passed arguments is not an assoc array. Ex. Template::Get("index.phtml", array("key"=>"value")');
+			}else{
+				extract($args);
+			}
+		}else if(is_object($args)){
+			$name = get_class($args);
+			Notice::Set("Object is not correspond. ($name)");
+		}
+
+		//	...
+		if(!file_exists($path)){
+			Notice::Set("File is not exists. ($path)");
+			return null;
+		}
+		d($path);
+		include($path);
+		return ob_get_clean();
+	}
+}
