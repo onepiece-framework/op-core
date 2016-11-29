8b8bbd45399cd3125adbd067627634bfd0910558
Tomoaki Nagahara <1668339+TomoakiNagahara@users.noreply.github.com>
2016-11-29 18:41:00 +0900

    New: Time - We call "ICE AGE".

---
+++ Time.class.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Time.class.php
+ *
+ * This class is part of the "New World".
+ * We call "ICE AGE".
+ * "ICE AGE" is very simple, but very powerful.
+ *
+ * @creation  2016-11-17
+ * @version   1.0
+ * @package   core7
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright 2016 (C) Tomoaki Nagahara All right reserved.
+ */
+
+/**
+ * Time
+ *
+ * @creation  2016-11-17
+ * @version   1.0
+ * @package   core7
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright 2016 (C) Tomoaki Nagahara All right reserved.
+ */
+class Time extends OnePiece
+{
+	/**
+	 * Frozen of world time.
+	 *
+	 * @var integer
+	 */
+	static private $_time;
+
+	/**
+	 * Timezone.
+	 *
+	 * @var string
+	 */
+	static private $_timezone;
+
+	/**
+	 * Return date. (not include time)
+	 *
+	 * @return string
+	 */
+	static function Date()
+	{
+		return date('Y-m-d', self::Get());
+	}
+
+	/**
+	 * Return date and time.
+	 *
+	 * @return string
+	 */
+	static function Datetime()
+	{
+		return date('Y-m-d H:i:s', self::Get());
+	}
+
+	/**
+	 * Get frozen time.
+	 */
+	static function Get()
+	{
+		if(!self::$_time){
+			self::$_time = strtotime(gmdate('Y-m-d H:i:s'));
+		}
+		return self::$_time;
+	}
+
+	/**
+	 * Get/Set Timezone.
+	 *
+	 * @param  null|string $timezone
+	 * @return null|string
+	 */
+	static function Timezone($timezone=null)
+	{
+		if( $timezone ){
+			date_default_timezone_set($timezone);
+		}else{
+			return self::$_timezone;
+		}
+	}
+}
