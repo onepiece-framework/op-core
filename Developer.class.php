6633130fafefbb7b4f302395277d701fc87f9574
Tomoaki Nagahara <1668339+TomoakiNagahara@users.noreply.github.com>
2017-01-26 15:12:48 +0900

    Del: Developer - Notice()

--- Developer.class.php
+++ Developer.class.php
@@ -140,28 +140,6 @@ class Developer extends OnePiece
 	}
 
 	/**
-	 * Show notice message for developer.
-	 *
-	 * @param array $notice
-	 */
-	static function Notice($notice)
-	{
-		//	Output is first time only.
-		static $is_notice;
-		if(!$is_notice ){
-			$is_notice = true;
-			global $_OP;
-			$op_root  = $_OP[OP_ROOT];
-			$app_root = $_OP[APP_ROOT];
-			$doc_root = $_OP[DOC_ROOT];
-			print "<div id=\"OP_NOTICE\" data-OP_ROOT=\"{$op_root}\" data-APP_ROOT=\"{$app_root}\" data-DOC_ROOT=\"{$doc_root}\"></div>".PHP_EOL;
-			print '<script type="text/javascript" src="/js/Notice.js"></script>'.PHP_EOL;
-			print '<link rel="stylesheet" type="text/css" href="/css/Notice.css">'.PHP_EOL;
-		}
-		print '<div class="OP_NOTICE">'.self::_toJson($notice).'</div>';
-	}
-
-	/**
 	 * Send admin notice mail.
 	 *
 	 * @param array
