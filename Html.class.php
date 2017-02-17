86a5f8f938dda5db1957eae6bb04462a65abcf83
Tomoaki Nagahara <1668339+TomoakiNagahara@users.noreply.github.com>
2017-02-17 22:47:29 +0900

    NEW: Html

---
+++ Html.class.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Html.class.php
+ *
+ * @creation  2017-02-17
+ * @version   1.0
+ * @package   core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/**
+ * Html
+ *
+ * @creation  2017-02-17
+ * @version   1.0
+ * @package   core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Html
+{
+	/** trait.
+	 *
+	 */
+	use OP_CORE;
+
+	/** Output P tag.
+	 *
+	 * @param string $text
+	 */
+	static function P($text)
+	{
+		//	...
+		$text = Escape($text);
+
+		//	...
+		$class = get_called_class();
+
+		//	...
+		$class::Tag(__FUNCTION__, $text);
+	}
+
+	/** Output P tag.
+	 *
+	 * @param string $text
+	 */
+	static function Span($text)
+	{
+		//	...
+		$text = Escape($text);
+
+		//	...
+		$class::Tag(__FUNCTION__, $text);
+	}
+
+	/** Output P tag.
+	 *
+	 * @param string $text
+	 */
+	static function Tag($tag, $text)
+	{
+		//	...
+		$tag = strtolower($tag);
+
+		//	...
+		$text = Escape($text);
+
+		//	...
+		echo "<{$tag}>{$text}</{$tag}>".PHP_EOL;
+	}
+}
