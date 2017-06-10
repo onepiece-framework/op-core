d2c61cdaa0e2fddb0a623b6b1ed5734191273ea5
Tomoaki Nagahara <1668339+TomoakiNagahara@users.noreply.github.com>
2017-06-10 23:24:28 +0900

    New: Template/Js/Functions.js - ConvertPath()

--- Template/Js/Functions.js
+++ Template/Js/Functions.js
@@ -7,7 +7,6 @@
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
-
 //	...
 function CompressPath( path ){
 	//	...
@@ -23,5 +22,28 @@ function CompressPath( path ){
 		}
 	};
 
+	return path;
+}
+
+//	...
+function ConvertPath( path ){
+	//	...
+	if(!__meta_root__){
+		return;
+	}
+
+	//	...
+	var m = path.match(/^([\w]+):/);
+	if( m[1] ){
+		//	...
+		var r = new RegExp('^'+m[1]+':/');
+		path = path.replace(r,__meta_root__[m[1]]);
+
+		//	...
+		var r = new RegExp('^'+__meta_root__.doc);
+		path = path.replace(r,'/');
+	}
+
+	//	...
 	return path;
 }
