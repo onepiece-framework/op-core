8dff105915a77d0aeb996c41a061ef9861d02282
developer's app1 <uqunie@app1.uqunie.com>
2017-08-29 16:26:20 +0900

    New: Functions.php - _GetRootsPath(), Added to unit dir.

--- Functions.php
+++ Functions.php
@@ -16,6 +16,7 @@
 function _GetRootsPath()
 {
 	global $_OP;
+	$root['unit:/']= realpath(Env::Get(Unit::_DIRECTORY_));
 	$root['app:/'] = $_OP['APP_ROOT'];
 	$root['doc:/'] = $_OP['DOC_ROOT'];
 	$root['op:/']  = $_OP['OP_ROOT'];
