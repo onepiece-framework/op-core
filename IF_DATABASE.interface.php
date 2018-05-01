aecf1d407b1b3e8b5c0e013ae40bd5d16c9b7557
Tomoaki -imac5k- <1668339+TomoakiNagahara@users.noreply.github.com>
2018-05-01 11:38:23 +0900

    NEW: IF_DATABASE, IF_FORM, IF_SQL

---
+++ IF_DATABASE.interface.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * IF_DATABASE.interface.php
+ *
+ * @creation  2018-04-20
+ * @version   1.0
+ * @package   core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** IF_DATABASE
+ *
+ * @creation  2018-04-20
+ * @version   1.0
+ * @package   core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+interface IF_DATABASE
+{
+	public function PDO();
+	public function Connect($config);
+	public function Insert($config);
+	public function Select($config);
+	public function Update($config);
+	public function Delete($config);
+	public function Quick($config);
+	public function Query($config);
+	public function Queries($config);
+}
---
+++ IF_FORM.interface.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * IF_FORM.interface.php
+ *
+ * @creation  2018-04-20
+ * @version   1.0
+ * @package   core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** IF_FORM
+ *
+ * @creation  2018-04-20
+ * @version   1.0
+ * @package   core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+interface IF_FORM
+{
+	public function Start();
+	public function Finish();
+	public function Input();
+	public function Value();
+	public function Label();
+}
