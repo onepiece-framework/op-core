2d1a66c80691df47a19db38f0ca6f1347fc2b134
Tomoaki macbook12i7 <1668339+TomoakiNagahara@users.noreply.github.com>
2020-05-03 20:36:54 +0900

    New: include > isAdmin.php | Use Env::isAdmin()

---
+++ include/isAdmin.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * include/isAdmin.php
+ *
+ * @created   2020-04-29
+ * @version   1.0
+ * @package   core
+ * @author    Tomoaki Nagahara<tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+if( self::isLocalhost() ){
+	return true;
+}
+
+//	...
+$remote_addr = $_SERVER['REMOTE_ADDR'] ?? false;
+
+//	...
+if( $remote_addr === ($_SERVER['ADMIN_IP'] ?? null) ){
+	return true;
+}
+
+//	...
+if( $remote_addr === (self::$_env[self::_ADMIN_IP_] ?? null) ){
+	return true;
+}
+
+//	...
+return false;
