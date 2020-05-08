b87c43df6c82526c4a780727bb8fd3265b3bdead
Tomoaki mbp13r-2015 <1668339+TomoakiNagahara@users.noreply.github.com>
2020-05-08 17:35:03 +0900

    New: function > GetExtension.php

---
+++ function/GetExtension.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * op-core:/function/GetExtension.php
+ *
+ * @created   2020-05-08
+ * @package   op-core
+ * @version   1.0
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** Get extension.
+ *
+ * @param  string $file
+ * @return string $extension
+ */
+function GetExtension(string $file):string
+{
+	//	...
+	if(!$pos = strrpos($file, '.') ){
+		return false;
+	}
+
+	//	...
+	return substr($file, $pos+1);
+}
