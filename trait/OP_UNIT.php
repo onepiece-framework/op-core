c5fafaab0ff0687ec32fdfe18dc2d905b690d4cb
Tomoaki mbp13r-2015 <1668339+TomoakiNagahara@users.noreply.github.com>
2020-05-11 00:53:30 +0900

    Del: trait > OP_UNIT > Config()

--- trait/OP_UNIT.php
+++ trait/OP_UNIT.php
@@ -25,46 +25,6 @@ namespace OP;
  */
 trait OP_UNIT
 {
-	/** Config
-	 *
-	 * @created   2019-12-13   Moved from Env::Get().
-	 */
-	static function Config($config=null)
-	{
-		//	...
-		static $_config;
-
-		//	Get class name.
-		$class_name = self::_ClassName();
-
-		//	Force lower case.
-		$class_name = strtolower($class_name);
-
-		//	Check by class name whether config is initialized.
-		if( empty($_config[$class_name]) ){
-			//	Since config is empty, load the file if it exists.
-			if( file_exists($path = ConvertPath("asset:/config/{$class_name}.php")) ){
-				//	Since the file exists, it is loaded only once.
-				$_config[$class_name] = require_once($path);
-			}
-		}
-
-		/** About array merge.
-		 *
-		 *  array_replace_recursive() is all replace.
-		 *  array_merge_recursive() is number index is renumbering.
-		 *
-		 * @copied    2019-12-13   Moved from Env::Set().
-		 */
-		//	self::$_env[$key] = array_merge_recursive(self::$_env[$key], $var);
-		if( $config ){
-			$_config[$class_name] = array_replace_recursive($_config[$class_name], $config);
-		}
-
-		//	...
-		return $_config[$class_name];
-	}
-
 	/** Unit
 	 *
 	 *  Always return instantiated instance.
