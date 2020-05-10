b6e96e3a2c4b8052abaeba0c0f93d3bf0af399b9
Tomoaki mbp13r-2015 <1668339+TomoakiNagahara@users.noreply.github.com>
2020-05-10 15:28:54 +0900

    New: function > Template.php

---
+++ function/Template.php
@@ -0,0 +1,57 @@
+<?php
+/** Template.php
+ *
+ * @created   2020-05-10
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** Template
+ *
+ * @created   2020-05-10
+ * @param     string       $function
+ * @param     array        $args
+ * @throws   \Exception
+ */
+function Template(string $path, array $args=[]):void
+{
+	//	Convert path.
+	$path = ConvertPath($path);
+
+	//	Save current directory.
+	$current_dir = getcwd();
+
+	//	Change to execute file in directory.
+	chdir( dirname($path) );
+
+	//	Get file name.
+	$name = basename($path);
+
+	//	Sandbox
+	call_user_func(function($name, $args){
+
+		//	Swap file name.
+		$md5 = 'file_' . md5(microtime());
+		${$md5} = $name;
+
+		//	If variables passed.
+		if(!empty($args) ){
+			//	Extract passed variables.
+			extract($args, null, null);
+		};
+
+		//	Execute file.
+		include(${$md5});
+
+	}, $name, $args);
+
+	//	Recovery last directory.
+	chdir($current_dir);
+}
