90f478942d8dd47e366ab06dae240f213886a36f
Tomoaki mbp13r-2015 <1668339+TomoakiNagahara@users.noreply.github.com>
2020-05-10 15:30:12 +0900

    New: function > GetTemplate.php

---
+++ function/GetTemplate.php
@@ -0,0 +1,39 @@
+<?php
+/** GetTemplate.php
+ *
+ * @created   2020-05-10
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** Template
+ *
+ * @created   2020-05-10
+ * @param     string       $function
+ * @param     array        $args
+ * @throws   \Exception
+ * @return    string
+ */
+function GetTemplate(string $path, array $args=[]):string
+{
+	//	...
+	if(!ob_start()){
+		throw new \Exception('ob_start was failed.');
+	}
+
+	//	...
+	Load('Template');
+
+	//	...
+	Template($path, $args);
+
+	//	...
+	return ob_get_clean();
+}
