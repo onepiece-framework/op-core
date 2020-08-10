e99840a4aa94b0e9678cf4cad9ecf089d87d7e2d
Tomoaki macbook12i7 <1668339+TomoakiNagahara@users.noreply.github.com>
2020-08-10 17:47:53 +0900

    New: function > GetMimeFromExtension.php

---
+++ function/GetMimeFromExtension.php
@@ -0,0 +1,37 @@
+<?php
+/** op-core:/function/GetMimeFromExtension.php
+ *
+ * @created   2020-08-10
+ * @package   op-core
+ * @version   1.0
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** Get MIME from extension.
+ *
+ * @param  string $extension
+ * @return string $mime
+ */
+function GetMimeFromExtension(string $ext):string
+{
+	//	...
+	switch( $ext ){
+		case 'js':
+			$mime = 'text/javascript';
+			break;
+		case 'css':
+			$mime = 'text/css';
+			break;
+		default:
+			$mime = '';
+	};
+
+	//	...
+	return $mime;
+}
