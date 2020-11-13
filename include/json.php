dc6ce1c59c4da0bc9f9ce82670aa37f403faeaab
Tomoaki macbook12i7 <1668339+TomoakiNagahara@users.noreply.github.com>
2020-11-13 19:14:06 +0900

    New: include > json.php

--- include/json.php
+++ include/json.php
@@ -1,5 +1,21 @@
 <?php
+/** op-core:/include/json.php
+ *
+ * @created   2020-10-25
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
 
+/** namespace
+ *
+ */
+namespace OP;
+
+/** If json string submitted, Overwrite to $_POST.
+ *
+ */
 if(($_SERVER['CONTENT_TYPE'] ?? null) === 'application/json' ){
 	$input = file_get_contents("php://input");
 	$_POST = json_decode($input, true);
