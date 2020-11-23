4fe6278443df04d28ac759bac03cd7879b7e093c
Tomoaki macbook12i7 <1668339+TomoakiNagahara@users.noreply.github.com>
2020-11-23 13:45:37 +0900

    New: function > Markdown.php

---
+++ function/Markdown.php
@@ -0,0 +1,38 @@
+<?php
+/** op-core:/function/Markdown.php
+ *
+ * @created   2020-09-05
+ * @moved     2020-11-18   from uqunie.com
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** Args
+ *
+ * @created   2020-09-05   Born at uqunie.com
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+function Markdown($file){
+	/*
+	//	...
+	if(!file_exists($file) ){
+		D("This file does not exist. ($file)");
+		return;
+	}
+	*/
+
+	//	...
+	echo '<div class="markdown"><pre><code>';
+	Template($file);
+	echo '</code></pre></div>';
+}
