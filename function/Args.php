6042830c49a3592ba53960c55544c5374f36f784
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2020-12-30 17:17:31 +0900

    New: function > Args.php

---
+++ function/Args.php
@@ -0,0 +1,27 @@
+<?php
+/** op-core:/function/Args.php
+ *
+ * @created   2020-12-30
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** use
+ *
+ */
+
+/** Return \OP\Unit('Router')->Args();
+ *
+ * @created   2020-12-30
+ * @return    array        $args
+ */
+function Args(){
+	return \OP\Unit('Router')->Args();
+}
