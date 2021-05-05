b0acf17b3c550ee8cce12cc9b198335af534cee8
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2021-05-05 18:33:34 +0900

    NEW: testcase > rootpath.php, rootpath.md

---
+++ testcase/rootpath.php
@@ -0,0 +1,17 @@
+<?php
+/** op-core:/testcase/rootpath.php
+ *
+ * @created   2021-05-05
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+D( RootPath() );
