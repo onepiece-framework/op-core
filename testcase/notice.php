1d8c71fa98b502f914342fab3aecf99500c7e0db
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2021-05-05 18:33:11 +0900

    NEW: testcase > notice.php, notice.md

---
+++ testcase/notice.php
@@ -0,0 +1,17 @@
+<?php
+/** op-core:/testcase/notice.php
+ *
+ * @created   2021-05-05
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+Notice('This is test.');
