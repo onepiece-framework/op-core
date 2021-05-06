9c7b354ec6e90e8269ea99ccf45851ba4213c7e6
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2021-05-06 18:23:10 +0900

    NEW: testcase > i18n.php, i18n.md

---
+++ testcase/i18n.php
@@ -0,0 +1,18 @@
+<?php
+/** op-core:/testcase/i18n.php
+ *
+ * @created   2021-05-06
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+?>
+<p data-i18n="true" data-i18n-locale="en">This is i18n test.</p>
