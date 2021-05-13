a71dc9e3b1d33481ddde0297456ed2269caeef2d
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2021-05-13 12:42:07 +0900

    New: Config > _Fetch()

--- Config.class.php
+++ Config.class.php
@@ -54,6 +54,99 @@ class Config
 		return $name;
 	}
 
+	/** Fetch from config file.
+	 *
+	 * @created   2021-05-12
+	 * @param     string       $name
+	 */
+	static private function _Fetch($name)
+	{
+		//	Static variable.
+		static $_asset_root, $_config_dir;
+
+		//	Init static variable.
+		if(!$_asset_root ){
+			//	Get asset root path.
+			$_asset_root = RootPath('asset');
+
+			//	Generate config directory.
+			$_config_dir = "{$_asset_root}config/";
+		}
+
+		//	Initialize to avoid an infinite loop.
+		self::$_config[$name] = [];
+
+		//	Include closure function.
+		$include = function($path){ return include($path); };
+
+		//	Ignore "unit" config. --> Got to infinity loop.
+		if( $name !== 'unit' ){
+
+			//	Generate file path.
+			$path = $_asset_root . "unit/{$name}/config.php";
+
+			//	Check exists.
+			if( file_exists($path) ){
+				//	Load the config file that each unit has by default.
+				self::$_config[$name] = $include($path);
+			}
+		}
+
+		//	Get current directory.
+		$save_directory = getcwd();
+
+		//	Check if config directory exists.
+		if( file_exists($_config_dir) ){
+
+			//	Chenge config direcotry.
+			chdir($_config_dir);
+
+			//	Correspond to overwrite public config at privete local config.
+			//	  --> config.php --> _config.php
+			foreach([$name, "_{$name}"] as $file_name){
+				//	Check if file exists.
+				if( file_exists($path = "{$file_name}.php") ){
+					//	Include config.
+					$config = $include($path);
+
+					//	Check if an array.
+					if( gettype($config) !== 'array' ){
+						Notice::Set("This file does not return an array. `{$path}`");
+						continue;
+					}
+
+					/** About array merge.
+					 *
+					 *  array_replace_recursive() is all replace.
+					 *  array_merge_recursive() is number index is renumbering.
+					 *
+					 */
+					self::$_config[$name] = isset(self::$_config[$name]) ? array_replace_recursive(self::$_config[$name], $config) : $config;
+
+					//	Escape.
+					continue;
+				}
+
+				//	Check if under score file. --> _config.php
+				if( $file_name[0] === '_'  ){
+					continue;
+				}
+
+				//	Flags
+				$fail = true;
+			}
+		}
+
+		//	Recovery save direcotry.
+		chdir($save_directory);
+
+		//	...
+		if( $fail ?? null ){
+			$message = "This config file is not exists. ($name)";
+			E('file', $message);
+		}
+	}
+
 	/** Get
 	 *
 	 * @created   2019-12-27
