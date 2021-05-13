916d63212de05673917019814d2b4d182086365c
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2021-05-13 12:22:09 +0900

    NEW: testcase > userid.php

---
+++ testcase/userid.php
@@ -0,0 +1,17 @@
+<?php
+/** op-core:/testcase/userid.php
+ *
+ * @created   2021-05-12
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+D( Cookie::UserID() );
