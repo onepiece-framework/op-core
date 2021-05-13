91fc69f22a92bcda15308e7dfcbb5ba6b108d5a2
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2021-05-13 12:40:51 +0900

    Mov: Config > _Init() | fetch code --> _Fetch()

--- Config.class.php
+++ Config.class.php
@@ -41,97 +41,13 @@ class Config
 	 */
 	static private function _Init($name)
 	{
-		//	Static variable.
-		static $_asset_root, $_config_dir;
-
-		//	Init static variable.
-		if(!$_asset_root ){
-			//	Get asset root path.
-			$_asset_root = RootPath('asset');
-
-			//	Generate config directory.
-			$_config_dir = "{$_asset_root}config/";
-		}
-
 		//	Force lower case.
 		$name = strtolower($name);
 
 		//	Check by class name whether config is initialized.
 		if(!isset(self::$_config[$name]) ){
-
-			//	Initialize to avoid an infinite loop.
-			self::$_config[$name] = [];
-
-			//	Include closure function.
-			$include = function($path){ return include($path); };
-
-			//	Ignore "unit" config. --> Got to infinity loop.
-			if( $name !== 'unit' ){
-
-				//	Generate file path.
-				$path = $_asset_root . "unit/{$name}/config.php";
-
-				//	Check exists.
-				if( file_exists($path) ){
-					//	Load the config file that each unit has by default.
-					self::$_config[$name] = $include($path);
-				}
-			}
-
-			//	Get current directory.
-			$save_directory = getcwd();
-
-			//	Check if config directory exists.
-			if( file_exists($_config_dir) ){
-
-			//	Chenge config direcotry.
-			chdir($_config_dir);
-
-			//	Correspond to overwrite public config at privete local config.
-			//	  --> config.php --> _config.php
-			foreach([$name, "_{$name}"] as $file_name){
-				//	Check if file exists.
-				if( file_exists($path = "{$file_name}.php") ){
-					//	Include config.
-					$config = $include($path);
-
-					//	Check if an array.
-					if( gettype($config) !== 'array' ){
-						Notice::Set("This file does not return an array. `{$path}`");
-						continue;
-					}
-
-					/** About array merge.
-					 *
-					 *  array_replace_recursive() is all replace.
-					 *  array_merge_recursive() is number index is renumbering.
-					 *
-					 */
-					self::$_config[$name] = isset(self::$_config[$name]) ? array_replace_recursive(self::$_config[$name], $config) : $config;
-
-					//	Escape.
-					continue;
-				}
-
-				//	Check if under score file. --> _config.php
-				if( $file_name[0] === '_'  ){
-					continue;
-				}
-
-				//	Flags
-				$fail = true;
-			}
-			}
-
-			//	Recovery save direcotry.
-			chdir($save_directory);
-
-			//	...
-			if( $fail ?? null ){
-				$message = "This config file is not exists. ($name)";
-				Debug::Set('file', $message);
-			//	Notice::Set($message);
-			}
+			//	Fetch from config file.
+			self::_Fetch($name);
 		}
 
 		//	...
