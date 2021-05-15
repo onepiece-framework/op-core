0988f473a90e3840a42227e6ccbdec2c9e45a536
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2021-05-15 23:49:23 +0900

    NEW: Session.class.php

---
+++ Session.class.php
@@ -0,0 +1,61 @@
+<?php
+/** op-core:/Session.class.php
+ *
+ * @created   2021-05-15
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** Session
+ *
+ * <pre>
+ * //  Save the value in the session.
+ * $val = Session::Set('key', 'saved value');
+ *
+ * //  Gets the value saved in the session.
+ * $val = Session::Get('key', 'default value, in case of null');
+ * </pre>
+ *
+ * @created   2021-05-15
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Session
+{
+	/** trait.
+	 *
+	 */
+	use OP_CORE, OP_SESSION;
+
+	/** Get session value.
+	 *
+	 * @created   2021-05-15
+	 * @param     string       $key
+	 * @param     mixed        $default
+	 * @return    mixed
+	 */
+	static function Get(string $key, $default)
+	{
+		return self::Session()[$key] ?? $default;
+	}
+
+	/** Set session value.
+	 *
+	 * @created   2021-05-15
+	 * @param     string       $key
+	 * @param     mixed        $val
+	 */
+	static function Set(string $key, $val)
+	{
+		self::Session()[$key] = $val;
+	}
+}
