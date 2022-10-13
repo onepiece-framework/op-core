df84cef4f2e6296e914338630c570a3dc2a3b45c
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-10-13 15:58:28 +0900

    CI: ci > Browser.php, Config.php

---
+++ ci/Browser.php
@@ -0,0 +1,23 @@
+<?php
+/** op-core:/ci/Browser.php
+ *
+ * @created   2022-10-12
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$config = [];
+
+//	...
+$config['Mac'][] = ['result' => true];
+
+//	...
+return $config;
