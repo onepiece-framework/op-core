e8c9c91e05f2daf2e2635a93d7f862399d1ab905
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2022-10-13 20:12:10 +0900

    Sandbox: testcase > sandbox.php

---
+++ testcase/sandbox.php
@@ -0,0 +1,25 @@
+<?php
+/** op-core:/testcase/sandbox.php
+ *
+ * @created   2022-10-12
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$int  =  1;
+$str  = '1';
+$bool = true;
+
+//	...
+$result = OP::Sandbox('sandbox.phtml', $int, $str, $bool, null);
+
+//	...
+D($result);
