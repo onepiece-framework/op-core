48f5d7fd6c75b7f4c1098b849ff5acf42c2f373b
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2022-10-13 15:58:28 +0900

    CI: ci > Browser.php, Config.php

---
+++ ci/Config.php
@@ -0,0 +1,26 @@
+<?php
+/** op-core:/ci/Config.php
+ *
+ * @created   2022-10-12
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$config = [];
+
+//	...
+$config['_Init'][]  = ['result' => 'ci', 'args' => 'CI'];
+$config['_Fetch'][] = ['result' => null, 'args' => 'CI'];
+$config['Get'][]    = ['result' => ['execute' => true],  'args' => 'CI'];
+$config['Set'][]    = ['result' => ['execute' => false], 'args' => ['CI', ['execute'=>false]]];
+
+//	...
+return $config;
