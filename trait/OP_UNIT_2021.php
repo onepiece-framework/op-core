da89b2e546b251e895a5998f5d36553e92ab033c
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-10-13 23:13:46 +0900

    New: trait > OP_UNIT_2021

---
+++ trait/OP_UNIT_2021.php
@@ -0,0 +1,27 @@
+<?php
+/** op-core:/OP_UNIT_2021.php
+ *
+ * @created   2022-10-05
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** OP_UNIT
+ *
+ * @created   2019-02-21
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+trait OP_UNIT_2021
+{
+
+}
