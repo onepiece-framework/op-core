440c40adabff9d12d88eca98e817244ea9a983af
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-10-20 12:23:51 +0900

    CI: ci > CI.php

---
+++ ci/CI.php
@@ -0,0 +1,30 @@
+<?php
+/** op-core:/ci/CI.php
+ *
+ * @created   2022-10-18
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$ci = new CI();
+
+//	Set
+$result =  null;
+$args   = ['Test', [], []];
+$ci->Set('Set', $result, $args);
+
+//	GenerateConfig
+$result = ['Test' => [['result' => [], 'args' => []]]];
+$args   = null;
+$ci->Set('GenerateConfig', $result, $args);
+
+//	...
+return $ci->GenerateConfig();
