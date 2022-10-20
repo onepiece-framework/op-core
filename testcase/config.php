de30b8f9eea2ae819af2c6c1be19964a12077ea3
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2022-10-20 12:24:36 +0900

    New: testcase > config.php, config.md

---
+++ testcase/config.php
@@ -0,0 +1,17 @@
+<?php
+/** op-core:/testcase/config.php
+ *
+ * @created   2021-10-18
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+D( Config::Get('Hoge') );
