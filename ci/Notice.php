d7a1bc8dad5bff4373f6aed7c72de18fbff287cd
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2022-11-01 19:08:03 +0900

    CI: ci > Notice.php

---
+++ ci/Notice.php
@@ -0,0 +1,45 @@
+<?php
+/** op-core:/ci/Notice.php
+ *
+ * @created   2022-11-01
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$ci = new CI();
+
+//	_Session
+$args   = null;
+$result = [];
+$ci->Set('_Session', $result, $args);
+
+//	Get
+$args   = null;
+$result = [];
+$ci->Set('Get', $result, $args);
+
+//	Pop
+$args   = null;
+$result = [];
+$ci->Set('Pop', $result, $args);
+
+//	Set
+$args   = ['This is notice message.',['backtrace']];
+$result =  'This is notice message.';
+$ci->Set('Set', $result, $args);
+
+//	Has
+$args   = null;
+$result = false;
+$ci->Set('Has', $result, $args);
+
+//	...
+return $ci->GenerateConfig();
