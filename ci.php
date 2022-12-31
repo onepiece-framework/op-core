41c9d7b81ab42aaf207fcb9ce056c289c67e1400
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-01-01 01:11:43 +0900

    CI: ci.php

---
+++ ci.php
@@ -0,0 +1,22 @@
+<?php
+/** op-core:/ci.php
+ *
+ * @created   2023-01-01
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+// require('../../ci.php');
