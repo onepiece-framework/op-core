8d19ad5741f721dac5f1aa2e01638b98e9b6bb1b
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-01 01:05:02 +0900

    New: testcase > email.php, email.txt

---
+++ testcase/email.php
@@ -0,0 +1,14 @@
+<?php
+/** op-core:/testcase/email.php
+ *
+ * @created   2023-01-01
+ * @version   1.0
+ * @package   op-core
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
