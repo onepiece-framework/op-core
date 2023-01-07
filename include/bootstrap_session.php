089f2e627dc370fe9cc00c88732d573126142030
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-07 23:03:27 +0900

    New: include > bootstrap_session.php <-- Bootstrap.php

---
+++ include/bootstrap_session.php
@@ -0,0 +1,60 @@
+<?php
+/** op-core:/include/bootstrap_session.php
+ *
+ *  1. Auto start session.
+ *  2. Separate session ID for each PHP version.
+ *  3. SameSite support.
+ *
+ * @created    2023-01-06
+ * @version    1.0
+ * @package    op-core
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** For SameSite
+ *
+ */
+//	Get current values.
+$params   = session_get_cookie_params();
+//	Check if https.
+$secure   = $_SERVER['REQUEST_SCHEME']==='https' ? true: false;
+//	Always true.
+$httponly = true;
+//	None is required for https.
+$samesite = $secure ? 'None':'Lax';
+
+//	Branch by PHP version.
+if( version_compare(PHP_VERSION, '7.3.0') <= 0 ){
+	//	Under 7.3
+	$params['path'] .= "; SameSite={$samesite}";
+	session_set_cookie_params($params['lifetime'], $params['path'], $params['domain'], $secure, $httponly);
+}else{
+	//	Upper 7.3
+	$params['secure']   = $secure;
+	$params['httponly'] = $httponly;
+	session_set_cookie_params($params);
+}
+
+//	Get default session name.
+$name = session_name();
+
+//	Added PHP version for run different versions of PHP-FPM at the same time.
+session_name($name .'_'. PHP_VERSION_ID);
+
+//	Start session.
+if(!session_start() ){
+	$line = __LINE__;
+	echo "asset:/core/Bootstrap.php #{$line} - Session start was failed.\n";
+	exit($line);
+}
