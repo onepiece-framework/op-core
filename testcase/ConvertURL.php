c6bf01825eaa2a1fa01d569e049b842cddce2ce3
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-01-11 23:10:09 +0900

    New: testcase > ConvertURL.php

---
+++ testcase/ConvertURL.php
@@ -0,0 +1,72 @@
+<?php
+/** op-module-testcase:/core/ConvertURL.php
+ *
+ * @creation  2019-04-04
+ * @version   1.0
+ * @package   op-module-testcase
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$app_root = ConvertURL('app:/');
+Html("app:/ --> $app_root");
+
+//	...
+$testcase_root = ConvertURL('testcase:/');
+Html("testcase:/ --> $testcase_root");
+
+//	...
+$asset_root = ConvertURL('asset:/');
+Html("asset:/ --> $asset_root");
+
+//	...
+$core_root = ConvertURL('core:/');
+Html("core:/ --> $core_root");
+
+//	...
+$op_root = ConvertURL('op:/');
+Html("op:/ --> $op_root");
+
+//	...
+return;
+
+//	Init.
+$results = [];
+
+//	Base URL
+$base_url = ConvertURL('testcase:/core/').'converturl';
+
+//	Check convert result.
+$results['standard']['REQUEST_URL'] = $_SERVER['REQUEST_URI'];
+$results['standard']['ConvertURL']  = $base_url;
+$results['standard']['result'] = (strpos($_SERVER['REQUEST_URI'], $base_url) === 0) ? true: false;
+
+//	Check slash of tail.
+$results['slash of tail'][] = ConvertURL('testcase:/');
+$results['slash of tail'][] = ConvertURL('testcase:/core');  // <-- Is directory.
+$results['slash of tail'][] = ConvertURL('testcase:/core/'); // <-- Is directory.
+$results['slash of tail'][] = ConvertURL('testcase:/core/converturl' ); // <-- Not directory.
+$results['slash of tail'][] = ConvertURL('testcase:/core/converturl/');
+
+//	...
+$result = true;
+
+//	...
+foreach( $results['slash of tail'] as $index => $value ){
+	$evalu = ($index === 3) ? false: true;
+	$slash = $value[strlen($value)-1] === '/';
+	if( $evalu !== $slash ){
+		$result =  false;
+		Notice("Slash of tail is unmatch. ($index, $value)");
+	}
+}
+$results['slash of tail']['result'] = $result;
+
+//	Result.
+D($results);
