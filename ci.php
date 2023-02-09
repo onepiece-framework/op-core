60129b8a8428d02ac22b482b199769f3247aeebb
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-02-09 18:58:47 +0900

    Fix: ci.php | v2.0 - require('ci.php') --> `php ci.php`

--- ci.php
+++ ci.php
@@ -2,7 +2,7 @@
 /** op-core:/ci.php
  *
  * @created   2023-01-01
- * @version   1.0
+ * @version   2.0
  * @package   op-core
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
@@ -19,4 +19,4 @@ declare(strict_types=1);
 namespace OP;
 
 //	...
-// require('../../ci.php');
+echo `php ../../ci.php`;
