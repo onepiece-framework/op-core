111a05f23c55be3d842c50307608614df0aa0601
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-02-11 08:19:35 +0900

    Del: CI > Root(), SubmoduleConfig(), CheckCommitID(), SaveCommitID(), CurrentBranchName(), CurrentCommitID()

--- ci/CI.php
+++ ci/CI.php
@@ -31,38 +31,5 @@ $result = ['Test' => [['result' => [], 'args' => []]]];
 $args   = null;
 $ci->Set('GenerateConfig', $result, $args);
 
-//	Root
-$args   = null;
-$result = OP()->MetaPath('app:/');
-$ci->Set('Root', $result, $args);
-
-//	SubmoduleConfig
-$args   = true;
-$result = CI::SubmoduleConfig();
-$ci->Set('SubmoduleConfig', $result, $args);
-
-//	CheckCommitID
-$file_name   = '.ci_commit_id_'.OP()->Unit('Git')->CurrentBranch();
-$commit_id_1 = OP()->Unit('Git')->CurrentCommitID();
-$commit_id_2 = file_exists($file_name) ? file_get_contents($file_name) : null;
-$args   = '';
-$result = ($commit_id_1 === $commit_id_2) ? true: false;
-$ci->Set('CheckCommitID', $result, $args);
-
-//	SaveCommitID
-$args   = null;
-$result = null;
-$ci->Set('SaveCommitID', $result, $args);
-
-//	CurrentBranchName
-$args   = '';
-$result = trim(`git rev-parse --abbrev-ref HEAD`);
-$ci->Set('CurrentBranchName', $result, $args);
-
-//	CurrentCommitID
-$args   = '';
-$result = trim(`git show --format='%H' --no-patch 2>&1`);
-$ci->Set('CurrentCommitID', $result, $args);
-
 //	...
 return $ci->GenerateConfig();
