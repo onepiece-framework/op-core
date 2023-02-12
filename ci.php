4a61f8203edd1e98f5d0dff1c15ed1d8d85c8b99
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-02-12 15:00:34 +0900

    Fix: ci.php | v2.2 - Now possible to run on each versions of PHP!

--- ci.php
+++ ci.php
@@ -2,7 +2,7 @@
 /** op-core:/ci.php
  *
  * @created    2023-01-01
- * @version    2.1
+ * @version    2.2
  * @package    op-core
  * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright  Tomoaki Nagahara All right reserved.
@@ -19,9 +19,12 @@ declare(strict_types=1);
 namespace OP;
 
 //	...
+$php = $_SERVER['_'];
+
+//	...
 $argv1 = $_SERVER['argv'][1] ?? '';
 $argv2 = $_SERVER['argv'][2] ?? '';
 $argv3 = $_SERVER['argv'][3] ?? '';
 $argv4 = $_SERVER['argv'][4] ?? '';
 $argv5 = $_SERVER['argv'][5] ?? '';
-echo `php ../../ci.php {$argv1} {$argv2} {$argv3} {$argv4} {$argv5}`;
+echo `{$php} ../../ci.php {$argv1} {$argv2} {$argv3} {$argv4} {$argv5}`;
