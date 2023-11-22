2f2979a932fb0678df38c2390bb6a6c37b4b9c7b
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-11-23 00:52:01 +0900

    Fix: ci > DebugBacktrace.php | Separated from real variable.

--- ci/DebugBacktrace.php
+++ ci/DebugBacktrace.php
@@ -22,23 +22,21 @@ namespace OP;
 $ci = OP::Unit('CI');
 
 //  Auto
-$backtraces = debug_backtrace();
-$args   = [];
-$result = 'core:/trait/OP_CI.php  66 - OP\DebugBacktrace::Auto()'.
-          'unit:/ci/CI.class.php 496 - OP\DebugBacktrace->CI_Inspection("Auto")'.
-          'unit:/ci/CI.class.php 412 - OP\UNIT\CI_Client::CI_Args(Unknow(object),"Auto",Unknow(array),null,null)'.
-          'unit:/ci/CI.class.php 383 - OP\UNIT\CI_Client::CI_Method(Unknow(object),"Auto",Unknow(array))'.
-          'unit:/ci/CI.class.php 319 - OP\UNIT\CI_Client::CI_Class(Unknow(object))'.
-          'unit:/ci/CI.class.php 239 - OP\UNIT\CI_Client->CI()'.
-          'unit:/ci/CI.class.php 118 - OP\UNIT\CI_Client->Auto()'.
-          'unit:/ci/CI.class.php  82 - OP\UNIT\CI->Single()'.
-          'unit:/ci/CI.class.php  52 - OP\UNIT\CI->All()'.
-          'app:/ci.php            50 - OP\UNIT\CI->Auto()';
+$backtrace  = [
+	'file'  => __FILE__,
+	'line'  => __LINE__,
+	'class' => 'OP\UNIT\CI',
+	'type'  => '->',
+	'function' => 'Auto',
+	'args'  => ['CI', 0, 1, true, false, null, 0.1, -1, OP()],
+];
+$backtraces[] = $backtrace;
+$args   = [$backtraces];
+$result = 'core:/ci/DebugBacktrace.php  27 - OP\UNIT\CI->Auto("CI",0,1,true,false,null,0.1,-1,OP\OP)';
 $ci->Set('Auto', $result, $args);
 
 //  Numerator
 $args   = [$backtraces[0]];
-$result = 'core:/function/Template.php  95 - include()';
 $ci->Set('Numerator', $result, $args);
 
 //  Args
