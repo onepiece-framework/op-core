2eedd869867eb4f7067c54c55f42a3375c7dd03d
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-01-22 17:17:53 +0900

    2023: interface > IF_WEBPACK.php

---
+++ interface/IF_WEBPACK.php
@@ -0,0 +1,26 @@
+<?php
+/** op-core:/IF_WEBPACK.php
+ *
+ * @created    2023-01-22
+ * @version    1.0
+ * @package    op-core
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** IF_APP
+ *
+ * @created    2023-01-22
+ * @version    1.0
+ * @package    op-core
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+interface IF_WEBPACK
+{
+}
