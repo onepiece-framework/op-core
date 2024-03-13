41599fd5315a7576bd87f40c88dae543c9d51098
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-03-14 04:46:51 +0900

    New: interface > IF_BITCOIN.php

---
+++ interface/IF_BITCOIN.php
@@ -0,0 +1,27 @@
+<?php
+/** op-core:/interface/IF_BITCOIN.php
+ *
+ * @created    2024-03-14
+ * @version    1.0
+ * @package    op-core
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** IF_BITCOIN
+ *
+ * @created    2024-03-14
+ * @version    1.0
+ * @package    op-core
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+interface IF_BITCOIN
+{
+
+}
