a8b8ae4449108f4282c01a079faab87446b239f4
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-04-07 17:42:54 +0900

    Fix: Functions.php | require() --> require_once()

--- Functions.php
+++ Functions.php
@@ -22,32 +22,32 @@ namespace OP;
  *
  * @created   2020-01-01
  */
-require(__DIR__.'/function/D.php');
-require(__DIR__.'/function/Load.php');
-require(__DIR__.'/function/Unit.php');
-require(__DIR__.'/function/Encode.php');
-require(__DIR__.'/function/Decode.php');
-require(__DIR__.'/function/Notice.php');
-require(__DIR__.'/function/Layout.php');
-require(__DIR__.'/function/RootPath.php');
-require(__DIR__.'/function/ConvertPath.php');
-require(__DIR__.'/function/ConvertURL-2.php');
-require(__DIR__.'/function/Template.php');
-require(__DIR__.'/function/GetTemplate.php');
-require(__DIR__.'/function/Content.php');
-require(__DIR__.'/function/CompressPath.php');
-require(__DIR__.'/function/Json.php');
-require(__DIR__.'/function/Html.php');
-require(__DIR__.'/function/Attribute.php');
-require(__DIR__.'/function/Args.php');
-require(__DIR__.'/function/ifset.php');
-require(__DIR__.'/function/Hasha1.php');
-require(__DIR__.'/function/Charset.php');
+require_once(__DIR__.'/function/D.php');
+require_once(__DIR__.'/function/Load.php');
+require_once(__DIR__.'/function/Unit.php');
+require_once(__DIR__.'/function/Encode.php');
+require_once(__DIR__.'/function/Decode.php');
+require_once(__DIR__.'/function/Notice.php');
+require_once(__DIR__.'/function/Layout.php');
+require_once(__DIR__.'/function/RootPath.php');
+require_once(__DIR__.'/function/ConvertPath.php');
+require_once(__DIR__.'/function/ConvertURL-2.php');
+require_once(__DIR__.'/function/Template.php');
+require_once(__DIR__.'/function/GetTemplate.php');
+require_once(__DIR__.'/function/Content.php');
+require_once(__DIR__.'/function/CompressPath.php');
+require_once(__DIR__.'/function/Json.php');
+require_once(__DIR__.'/function/Html.php');
+require_once(__DIR__.'/function/Attribute.php');
+require_once(__DIR__.'/function/Args.php');
+require_once(__DIR__.'/function/ifset.php');
+require_once(__DIR__.'/function/Hasha1.php');
+require_once(__DIR__.'/function/Charset.php');
 
 //	...
 if( _OP_APP_BRANCH_ < 2023 ){
-	require(__DIR__.'/function/Args.php');
-	require(__DIR__.'/function/ifset.php');
-	require(__DIR__.'/function/Hasha1.php');
-	require(__DIR__.'/function/Charset.php');
+	require_once(__DIR__.'/function/Args.php');
+	require_once(__DIR__.'/function/ifset.php');
+	require_once(__DIR__.'/function/Hasha1.php');
+	require_once(__DIR__.'/function/Charset.php');
 };
