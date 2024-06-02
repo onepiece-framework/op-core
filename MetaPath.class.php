b1cbe05286a89954f6ac21d91d5826935f7b5f30
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-06-02 17:01:44 +0900

    Fix: MetaPath.class.php | Remove garbage code.

--- MetaPath.class.php
+++ MetaPath.class.php
@@ -33,13 +33,6 @@ class MetaPath
 	 */
 	use OP_CORE, OP_CI;
 
-	/** Meta root path list.
-	 *
-	 * @created   2022-06-11
-	 * @var array
-	 */
-	static $_ROOTS = [];
-
 	/** Set meta root path.
 	 *
 	 * @created   2022-06-11
@@ -48,33 +41,8 @@ class MetaPath
 	 */
 	static function Set(string $meta, string $path)
 	{
-		//	...
 		require_once(__DIR__.'/function/RootPath.php');
 		return RootPath($meta, $path);
-
-		//	Check if full path. Does the path start at the route?
-		/* Why? Path start is root always.
-		if( $path[0] !== '/' ){
-			return false;
-		}
-		*/
-
-		//	Deny upper directory specify.
-		if( strpos($path, '../') !== false ){
-			throw new \Exception("Deny upper directory specify.");
-		}
-
-		//	Replace duplicate slash.
-		$path = preg_replace('|//|', '/', $path);
-
-		//	Add slash to head and tail.
-		$path = '/'.trim($path, '/').'/';
-
-		//	Save the meta label and root path.
-		self::$_ROOTS[$meta] = $path;
-
-		//	Succeeded.
-		return self::$_ROOTS[$meta];
 	}
 
 	/** Get path by meta label.
@@ -85,12 +53,8 @@ class MetaPath
 	 */
 	static function Get(string $meta) : ?string
 	{
-		//	...
 		require_once(__DIR__.'/function/RootPath.php');
 		return RootPath($meta);
-
-		//	...
-		return self::$_ROOTS[$meta] ?? null;
 	}
 
 	/** Get meta root path list.
@@ -103,9 +67,6 @@ class MetaPath
 		//	...
 		require_once(__DIR__.'/function/RootPath.php');
 		return RootPath();
-
-		//	...
-		return self::$_ROOTS;
 	}
 
 	/** Convert to the meta-path from the full-path.
@@ -119,31 +80,6 @@ class MetaPath
 		//	...
 		require_once(__DIR__.'/function/CompressPath.php');
 		return CompressPath($path);
-
-		//	Replace duplicate slash.
-		$path = preg_replace('|//|', '/', $path);
-
-		//	...
-		foreach( self::$_ROOTS as $label => $root ){
-			//	...
-			$pos = strpos($path, $root);
-
-			//	...
-			if( $pos === 0 ){
-				break;
-			}
-		}
-
-		//	...
-		if(!$pos !== 0 ){
-			return false;
-		}
-
-		//	...
-		$trim = substr($path, $pos);
-
-		//	...
-		return $label . ':/' . trim($trim,'/') . is_dir($path) ? '/':'';
 	}
 
 	/** Restore to the full-path from the meta-path.
@@ -157,35 +93,6 @@ class MetaPath
 		//	...
 		require_once(__DIR__.'/function/ConvertPath.php');
 		return ConvertPath($path, false);
-
-		/*
-		//	Replace duplicate slash.
-		$path = preg_replace('//*', '/', $path);
-		*/
-
-		/* @var $m array */
-		/*
-		if(!preg_match('|^(\w+):/|', $path, $m)){
-			E("Does not match meta label --> `$path`");
-			return false;
-		}
-		*/
-
-		/*
-		//	Get the meta-label.
-		$label = $m[1];
-
-		//	Get the root-path.
-		if(!$root = self::$_ROOTS[$label] ?? null ){
-			return null;
-		}
-
-		//	...
-		$len = strlen($label);
-
-		//	...
-		return $root . substr($path, $len);
-		*/
 	}
 
 	/** Convert to Document root URL from meta path and full path.
