614282af8943e52f233db7a7bdfc58dd5efb80b8
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-09-04 19:09:43 +0900

    New: interface > IF_LAYOUT.php

---
+++ interface/IF_LAYOUT.php
@@ -0,0 +1,26 @@
+<?php
+/** op-core:/interface/IF_LAYOUT.php
+ *
+ * @created    2024-06-12
+ * @version    1.0
+ * @package    op-core
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** IF_LAYOUT
+ *
+ * @created    2024-06-12
+ * @version    1.0
+ * @package    op-core
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+interface IF_LAYOUT
+{
+}
