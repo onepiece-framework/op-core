f9b85a2a2d8f64f788519bc17b3e1ec2ded9b9f1
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-10-22 16:59:36 +0900

    New: trait > OP_UNIT_MAPPER.php

---
+++ ci/Unit_Mapper.php
@@ -0,0 +1,21 @@
+<?php
+/** op-core:/ci/Unit_Mapper.php
+ *
+ * @created    2024-06-30
+ * @version    1.0
+ * @package    op-core
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci UNIT\CI\CI_Config */
---
+++ trait/OP_UNIT_MAPPER.php
@@ -0,0 +1,26 @@
+<?php
+/** op-core:/trait/OP_UNIT_MAPPER.php
+ *
+ * @created    2024-06-08
+ * @version    1.0
+ * @package    op-core
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** OP_UNIT_MAPPER
+ *
+ * @created    2024-06-08
+ * @version    1.0
+ * @package    op-core
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+trait OP_UNIT_MAPPER
+{
+}
