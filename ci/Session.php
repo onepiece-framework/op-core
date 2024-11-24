bb4235ec9c16ba21775659f92acaf31e13fe781d
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-11-24 20:32:53 +0900

    Fix: ci > Session.php | OP::Unit('CI') --> OP()->Unit()->CI()->Config()

--- ci/Session.php
+++ ci/Session.php
@@ -18,8 +18,8 @@ declare(strict_types=1);
  */
 namespace OP;
 
-//	...
-$ci = OP::Unit('CI');
+/* @var $ci \OP\UNIT\CI\CI_Config */
+$ci = OP()->Unit()->CI()->Config();
 
 //	Get
 $args   = 'self-check';
@@ -37,4 +37,4 @@ $result = ['self-check'=>true];
 $ci->Set('Session', $result, $args);
 
 //	...
-return $ci->GenerateConfig();
+return $ci->Get();
