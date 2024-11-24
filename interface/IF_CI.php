d1d5c659a8ee14b13f9ce68e2fac21f902e6d097
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-11-24 19:32:36 +0900

    New: interface > IF_CI.php

---
+++ interface/IF_CI.php
@@ -0,0 +1,19 @@
+<?php
+/** op-core:/interface/IF_CI.php
+ *
+ * @created    2024-11-24
+ * @version    1.0
+ * @package    op-core
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
